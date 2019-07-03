3c1226e8192d507e691c8d8cecd1dab5401230ee
mavnguye <mavnguye@ucsc.edu>
2019-07-03 15:36:15 -0700

    Clickable jbe icon

--- Source/components/pages/advising.js
+++ Source/components/pages/advising.js
@@ -1,7 +1,8 @@
 import React , {Component} from '../../node_modules/react';
-import { Button, Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
+import { TouchableHighlight, Button, Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
 import AppNavigator from '../../navigation/AppNavigator';
 import { createAppContainer } from 'react-navigation';
+import * as WebBrowser from 'expo-web-browser';
 
 export class AdvisingScreen extends React.Component {
   static navigationOptions = {
@@ -11,16 +12,23 @@ export class AdvisingScreen extends React.Component {
     const {navigate} = this.props.navigation;
     return (
     <View style= {styles.container}>
-        <Button
-        title="Jack Baskin School of Engineering"
-        color = "#e6f542"
-        onPress={() => navigate('JBE')}
-        />
-        
+    <TouchableHighlight onPress={jbe_home}>
+    <Image
+    source={require('../../assets/images/jbe_logo.png')}
+    style={{flex:1, height: undefined, width: undefined}}
+    resizeMode="contain"
+    />
+</TouchableHighlight>
     </View>
-    
+
     );
   }
+}
+
+function jbe_home() {
+  WebBrowser.openBrowserAsync(
+    'https://www.soe.ucsc.edu/'
+  );
 }
 
 const styles = StyleSheet.create({
@@ -29,6 +37,6 @@ const styles = StyleSheet.create({
       fontSize: 30,
       padding: 60,
       backgroundColor: "#4287f5",
-      flex: 1
+      flex: 0
     },
   });
