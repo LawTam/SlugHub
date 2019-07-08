86e6ba8e8fea73811f1b924393612c488c024972
Lawrence Tam <lawhtam@gmail.com>
2019-07-07 17:19:27 -0700

    advising and academic buttons only

--- Source/components/pages/Skeleton/homepage.js
+++ Source/components/pages/Skeleton/homepage.js
@@ -14,19 +14,33 @@ export class HomePageScreen extends React.Component {
     <View style= {styles.container}>
 
         <ImageBackground 
-          source={require('../../../assets/images/download.png')} 
-          resizeMode="contain"
+          source={require('../../../assets/images/UCSC_stock.jpg')} 
+          //resizeMode="contain"
           style={{width: '100%', height: '100%'}}>
 
-            <TouchableHighlight onPress={jbe_home}>
-              <Image
-              source={require('../../../assets/images/logo.jpg')}
-              style={{height: 100, width: 200}}
-              resizeMode="contain"
-              />
-            </TouchableHighlight>
+            <View style = {styles.advising_button}>
+              <TouchableHighlight onPress={() => navigate('Advising')}>
+                <Image
+                source={require('../../../assets/images/logo.jpg')}
+                style={
+                  {height: 100, width: 200}
+                }
+                resizeMode="contain"
+                />
+              </TouchableHighlight>
+            </View>
 
-            <Button
+            <View style = {styles.academic_button}>
+              <TouchableHighlight onPress={() => navigate('Academic')}>
+                <Image
+                source={require('../../../assets/images/robot-prod.png')}
+                style={{height: 100, width: 200}}
+                resizeMode="contain"
+                />
+              </TouchableHighlight>
+            </View>
+
+            {/* <Button
             title="Advising"
             color = "#e6f542"
             onPress={() => navigate('Advising')}
@@ -55,7 +69,7 @@ export class HomePageScreen extends React.Component {
             title="Canvas"
             color = "#e6f542"
             onPress={() => navigate('Canvas')}
-            />
+            /> */}
           </ImageBackground> 
     </View>
     
@@ -73,8 +87,21 @@ const styles = StyleSheet.create({
     container: {
       justifyContent: "center",
       fontSize: 30,
-      //padding: 60,
       backgroundColor: "#4287f5",
       flex: 1
     },
+
+    advising_button: {
+      top:  '5%',
+      left: '5%',
+      position: "absolute",
+    },
+
+    academic_button: {
+      top: '5%',
+      left: '55%',
+      position: "absolute",
+    }
+
+
   });
