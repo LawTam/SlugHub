8de84a90d6955c14ae5491b00b4bed19673612ed
Lawrence Tam <lawhtam@gmail.com>
2019-07-08 14:42:38 -0700

    get webpage function added

--- Source/components/pages/JackBaskin/Majors/cs_bs.js
+++ Source/components/pages/JackBaskin/Majors/cs_bs.js
@@ -8,11 +8,12 @@ import ScrollPicker from '../../../../node_modules/react-native-wheel-scroll-pic
 // npm install react-native-wheel-scroll-picker --save
 // npm i react-native-wheel-picker --save
 import Picker from '../../../../node_modules/react-native-wheel-picker'
+
 var PickerItem = Picker.Item;
 var dict = new Map();
 dict.set("CMPS 5J", "CMPS 5J: Intro to Programming");
-dict.set("CMPS 12A/L", "CMPS 12A/L: Accelerated Programming");
-dict.set("CMPS 12B/M", "CMPS 12B/M: Intro to Data Structures");
+dict.set("CMPS 12A", "CMPS 12A/L: Accelerated Programming");
+dict.set("CMPS 12B", "CMPS 12B/M: Intro to Data Structures");
 dict.set("CMPS 101", "CMPS 101: Algorithms and Abstract Data Types");
 
 export class CS_BSScreen extends React.Component {
@@ -20,7 +21,7 @@ export class CS_BSScreen extends React.Component {
 		super(props);
 		this.state = {
 			selectedItem : 0,
-			itemList: ["CMPS 5J", "CMPS 12A/L", "CMPS 12B/M", "CMPS 101", '黄忠', '马超', '魏延', '诸葛亮']
+			itemList: ["CMPS 5J", "CMPS 12A", "CMPS 12B", "CMPS 101"]
 		};
 	}
 
@@ -35,35 +36,47 @@ export class CS_BSScreen extends React.Component {
     var key = this.state.itemList[this.state.selectedItem]
     console.log(key);
     console.log(dict.get(key));
-  }
-  select = () => {
-    console.log(this.state.itemList);
-    var key = this.state.itemList[this.state.selectedItem]
-    console.log(key);
-    return this.dict.key[this.setState.selectedItem];
-  }
+	}
+
+	select = () => {
+		console.log(this.state.itemList);
+		var key = this.state.itemList[this.state.selectedItem]
+		console.log(key);
+		return this.dict.key[this.setState.selectedItem];
+	}
+
+	get_SOE_Webpage(class_key) {
+		//console.log("JBE webpage for: ", class_key)
+		classKey = class_key.replace(/\s/g, '');	// remove spaces
+		link = "https://courses.soe.ucsc.edu/courses/" + classKey
+		//console.log(link)
+
+		WebBrowser.openBrowserAsync(
+			link
+		);
+	}
 
 	render () {
 		return (
 			<View style={styles.container}>
 				<Text style={styles.welcome}>
-					Welcome to React Native!
+					Computer Science B.S. Courses
 				</Text>
 				<Picker style={{width: 150, height: 180}}
-          selectedValue={this.state.selectedItem}
+          			selectedValue={this.state.selectedItem}
 					itemStyle={{color:"white", fontSize:26}}
 					onValueChange={(index) => this.onPickerSelect(index)}>
-						{this.state.itemList.map((value, i) => (
-							<PickerItem label={value} value={i} key={"money"+value}/>
-						))}
+					{this.state.itemList.map((value, i) => (
+						<PickerItem label={value} value={i} key={"money"+value}/>
+					))}
 				</Picker>
 				<Text style={{margin: 20, color: '#ffffff'}}>
-          {dict.get(this.state.itemList[this.state.selectedItem])}
+          			{dict.get(this.state.itemList[this.state.selectedItem])}
 				</Text>
 
 				<Text style={{margin: 20, color: '#ffffff'}}
-						onPress={this.onAddItem}>
-			Search for this class!
+					onPress={() => this.get_SOE_Webpage(this.state.itemList[this.state.selectedItem])}>
+					Search for this class!
 				</Text>
 			</View>
 		);
