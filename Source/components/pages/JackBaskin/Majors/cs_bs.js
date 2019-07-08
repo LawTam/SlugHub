41ec08580559fa36d45a6146f2f62e6e9db4136d
Lawrence Tam <lawhtam@gmail.com>
2019-07-08 12:56:24 -0700

    dictionary changed in cs_bs

--- Source/components/pages/JackBaskin/Majors/cs_bs.js
+++ Source/components/pages/JackBaskin/Majors/cs_bs.js
@@ -1,67 +1,83 @@
-import React , {Component} from '../../../../node_modules/react';
+import React , {Component} from 'react';
 import { Button, Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
 import AppNavigator from '../../../../navigation/AppNavigator';
 import { createAppContainer } from 'react-navigation';
 import * as WebBrowser from 'expo-web-browser';
-import ScrollPicker from '../../../../node_modules/react-native-wheel-scroll-picker';
+//import ScrollPicker from '../../../../../node_modules/react-native-wheel-scroll-picker';
 // npm install styled-components
 // npm install react-native-wheel-scroll-picker --save
 // npm i react-native-wheel-picker --save
 import Picker from '../../../../node_modules/react-native-wheel-picker'
-var PickerItem = Picker.Item;
-var dict = new Map();
-dict.set("CMPS 5J", "CMPS 5J: Intro to Programming");
 
+var PickerItem = Picker.Item;
+var dict = {
+	"CMPS 5J": "CMPS 5J: Intro to Programming",
+	"CMPS 12A/L": "CMPS 12A/L: Accelerated Programming",
+	"CMPS 12B/L": "CMPS 12B/L: Intro to Data Structures",
+	"CMPS 101": "CMPS 101: Algorithms and Abstract Data Types",
+}
 
 export class CS_BSScreen extends React.Component {
   constructor(props) {
 		super(props);
 		this.state = {
-			selectedItem : 0,
-			itemList: ['CMPS 5J', '张飞', '关羽', '赵云', '黄忠', '马超', '魏延', '诸葛亮']
+			selectedIndex : 0,
+			itemList: ['CMPS 5J', 'CMPS 12A/L', 'CMPS 12B/L', 'CMPS 101', ]
 		};
 	}
 
 	onPickerSelect (index) {
 		this.setState({
-			selectedItem: index,
+			selectedIndex: index,
 		})
 	}
 
 	onAddItem = () => {
-    console.log(this.state.itemList);
-    var key = this.state.itemList[this.state.selectedItem]
-    console.log(key);
-    console.log(this.classes_dictionary[0]);
-  }
-  select = () => {
-    console.log(this.state.itemList);
-    var key = this.state.itemList[this.state.selectedItem]
-    console.log(key);
-    return this.classes_dictionary.key[0];
-  }
+		console.log(this.state.itemList);
+		var key = this.state.itemList[this.state.selectedItem]
+		console.log(key);
+		console.log(this.classes_dictionary[0]);
+	}
+
+	select = () => {
+		console.log(this.state.itemList);
+		var key = this.state.itemList[this.state.selectedItem]
+		console.log(key);
+		return this.classes_dictionary.key[0];
+	}
+
+	direct_url = () => {
+
+	}
 
 	render () {
 		return (
 			<View style={styles.container}>
+
 				<Text style={styles.welcome}>
-					Welcome to React Native!
+					Computer Science Course Offerings
 				</Text>
+
 				<Picker style={{width: 150, height: 180}}
-          selectedValue={this.state.selectedItem}
-					itemStyle={{color:"white", fontSize:26}}
-					onValueChange={(index) => this.onPickerSelect(index)}>
-						{this.state.itemList.map((value, i) => (
-							<PickerItem label={value} value={i} key={"money"+value}/>
-						))}
+          			selectedClass = {this.state.selectedIndex}
+					itemStyle = {{color:"white", fontSize:26}}
+					onValueChange = {(selectedClass) => this.onPickerSelect(selectedClass)}>
+
+					{this.state.itemList.map((value, i) => (
+						<PickerItem label={value} value={i} key={value}/>
+					))}
 				</Picker>
-				<Text style={{margin: 20, color: '#ffffff'}}>
-					{dict.get(this.state.itemList[this.state.selectedItem])}
+
+				<Text style={{margin: 20, color: '#ffffff'}} dictionary = {dict}>
+					{
+						this.state.itemList.map((value, i) => (
+						dict[this.state.selectedItem]))
+					}
 				</Text>
 
 				<Text style={{margin: 20, color: '#ffffff'}}
 						onPress={this.onAddItem}>
-			Search for this class!
+							Search for this class!
 				</Text>
 			</View>
 		);
