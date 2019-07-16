e475ca4144270282044695c37b2cdf32994e3590
Naum Markenzon <nmarkenz@ucsc.edu>
2019-07-16 12:29:46 -0700

    Refactored webscraping code that produces JSON files

--- SlugHubWebscraping/test.js
+++ SlugHubWebscraping/test.js
@@ -6,5 +6,27 @@ rest.get('http://127.0.0.1:5000/display').on('complete', function(result) {
     this.retry(5000); // try again after 5 sec
   } else {
     console.log(result);
+    create_dict(result);
   }
 });
+
+function create_dict(result) {
+ console.log(typeof result);
+    str_dict = JSON.stringify(result);
+    console.log(typeof str_dict);
+    console.log(str_dict);
+    //console.log(new_dict.get("CMPS 5J"));
+   var res = str_dict.split(",");
+    console.log(res);
+   //var str_class = res[0].split(" \" ");
+    //console.log(str_class);
+   var str2 = res[0].match(/[^\s"']+|"([^"]*)"/gmi)
+   console.log(str2);
+   console.log(str2[1]);
+   var dict = new Map();
+   dict.set(str2[1], str2[3]);
+   console.log(dict.get(str2[1]));
+
+}
+
+
