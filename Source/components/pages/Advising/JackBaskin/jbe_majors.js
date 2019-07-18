726eada2594e0d8b020c94ea00d0e08afbb87963
Zhelin Li <52444485+overlinl@users.noreply.github.com>
2019-07-18 15:40:17 -0700

    CS_BS major UI

--- Source/components/pages/Advising/JackBaskin/jbe_majors.js
+++ Source/components/pages/Advising/JackBaskin/jbe_majors.js
@@ -1,5 +1,5 @@
 import React , {Component} from 'react';
-import { TouchableHighlight, Button, Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
+import { TouchableHighlight, Button, Image, Platform, Text, StatusBar,StyleSheet, View, ScrollView, Animated, ImageBackground } from 'react-native';
 import AppNavigator from '../../../../navigation/AppNavigator';
 import { createAppContainer } from 'react-navigation';
 
@@ -22,59 +22,106 @@ export class JBE_MajorsScreen extends React.Component {
     const {navigate} = this.props.navigation;
     return (
       <View style= {styles.container}>
+      
+        
+      <ImageBackground 
+      source={require('../../../../assets/images/JBE_back_temp.png')}
+      style={{width: '100%', height: '100%'}}>
+        
+      <ScrollView 
+           scrollEventThrottle = { 16 }
+           contentContainerStyle = {{ paddingTop: 10}}
+           onScroll = { Animated.event(
+             [{ nativeEvent: { contentOffset: { y: this.AnimatedHeaderValue }}}]
+          )}>
+            
+      <TouchableHighlight 
+        style={styles.buttonContainer}
+        onPress={() => navigate('BE_page')}>
+          <Text
+            style={styles.buttonText}>
+              Bioengineering
+          </Text>
+      </TouchableHighlight>
 
-      <Button
-      title="Bioengineering"
-      color = "#e6f542"
-      onPress={() => navigate('BE_page')}
-      />
+      <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CE_Major')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Engineering
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CS_BS')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Science B.S.
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CS_BA')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Science B.A.
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CSGD_BS')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Science Game Design B.S.
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('EE_BS')}>
+           <Text
+             style={styles.buttonText}>
+               Electrical Engineering
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('NDT_BA')}>
+           <Text
+             style={styles.buttonText}>
+               Network and Digital Technology B.A. 
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('RE_BS')}>
+           <Text
+             style={styles.buttonText}>
+               Robotics Engineering B.S.
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('TIM_BS')}>
+           <Text
+             style={styles.buttonText}>
+              Technology and Information Management B.S.
+           </Text>
+       </TouchableHighlight>
+       
+       {/* <View style= {styles.orgs_container}>
 
-      <Button
-      title="Computer Engineering"
-      color = "#e6f542"
-      onPress={() => navigate('CE_Major')}
-      />
+       </View> */}
 
-      <Button
-      title="Computer Science B.S."
-      color = "#e6f542"
-      onPress={() => navigate('CS_BS')}
-      />
-
-      <Button
-      title="Computer Science B.A."
-      color = "#B22222"
-      onPress={() => navigate('CS_BA')}
-      />
-
-      <Button
-      title="Computer Science Game Design B.S."
-      color = "#DC143C"
-      onPress={() => navigate('CSGD_BS')}
-      />
-
-      <Button
-      title="Electrical Engineering"
-      color = "#e6f542"
-      onPress={() => navigate('EE_BS')}
-      />
-      <Button
-      title="Network and Digital Technology B.A. "
-      color = "#e6f542"
-      onPress={() => navigate('NDT_BA')}
-      />
-      <Button
-      title="Robotics Engineering B.S."
-      color = "#e6f542"
-      onPress={() => navigate('RE_BS')}
-      />
-      <Button
-      title="Technology and Information Management B.S."
-      color = "#e6f542"
-      onPress={() => navigate('TIM_BS')}
-      />
-
-
+      </ScrollView>
+      </ImageBackground>
       </View>
 
     );
@@ -84,9 +131,37 @@ export class JBE_MajorsScreen extends React.Component {
 const styles = StyleSheet.create({
   container: {
     justifyContent: "center",
-    fontSize: 30,
-    padding: 60,
-    backgroundColor: "#4287f5",
     flex: 1
+  },
+  buttonContainer: {
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 30,
+    marginRight: 30,
+    marginTop: 10,
+    marginBottom: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+},
+  buttonText: {
+    textAlign: 'center',
+    color: '#000000',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  orgs_container: {
+    justifyContent: "center",
+    backgroundColor: "#4287f5",
+    flex: .2,
+    borderBottomWidth: 400,
   },
 });
--- Source/components/pages/Advising/JackBaskin/jbe_minors.js
+++ Source/components/pages/Advising/JackBaskin/jbe_minors.js
@@ -1,5 +1,5 @@
 import React , {Component} from '../../../../node_modules/react';
-import { TouchableHighlight, Button, Image, Platform, Text, StatusBar,StyleSheet, View } from 'react-native';
+import { TouchableHighlight, Button, Image, Platform, Text, StatusBar,StyleSheet, View, ScrollView, Animated } from 'react-native';
 import AppNavigator from '../../../../navigation/AppNavigator';
 import { createAppContainer } from 'react-navigation';
 
@@ -18,62 +18,119 @@ export class JBE_MinorsScreen extends React.Component {
       title: 'Minors',
     };
   };
+
   render() {
     const {navigate} = this.props.navigation;
     return (
-    <View style= {styles.container}>
-        <Button
-        title="Computer Science"
-        color = "#FF0000"
-        onPress={() => navigate('CS_Minor')}
-        />
+      <View style= {styles.container}>
+      <ScrollView 
+           scrollEventThrottle = { 16 }
+           contentContainerStyle = {{ paddingTop: 10 }}
+           onScroll = { Animated.event(
+             [{ nativeEvent: { contentOffset: { y: this.AnimatedHeaderValue }}}]
+          )}>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CS_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Science
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('CE_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Computer Engineering
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('BioInfo_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Bioinformatics
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('AM_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Applied Math
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('EE_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Electrical Engineering
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('Statistics_Minor')}>
+           <Text
+             style={styles.buttonText}>
+               Statistics
+           </Text>
+       </TouchableHighlight>
+    
+       <TouchableHighlight 
+         style={styles.buttonContainer}
+         onPress={() => navigate('TIM_Minor')}>
+           <Text
+             style={styles.buttonText}>
+              Technology and Information Management
+           </Text>
+       </TouchableHighlight>
 
-        <Button
-        title="Computer Engineering"
-        color = "#e6f542"
-        onPress={() => navigate('CE_Minor')}
-        />
-
-        <Button
-        title="Bioinformatics"
-        color = "#e6f542"
-        onPress={() => navigate('BioInfo_Minor')}
-        />
-
-        <Button
-        title="Applied Math"
-        color = "#FF7F00"
-        onPress={() => navigate('AM_Minor')}
-        />
-
-        <Button
-        title="Electrical Engineering"
-        color = "#e6f542"
-        onPress={() => navigate('EE_Minor')}
-        />
-        <Button
-        title="Statistics"
-        color = "#e6f542"
-        onPress={() => navigate('Statistics_Minor')}
-        />
-        <Button
-        title="Technology and Information Management"
-        color = "#e6f542"
-        onPress={() => navigate('TIM_Minor')}
-        />
-
-    </View>
+      </ScrollView>
+      </View>
 
     );
   }
 }
 
 const styles = StyleSheet.create({
-    container: {
-      justifyContent: "center",
-      fontSize: 30,
-      padding: 60,
-      backgroundColor: "#4287f5",
-      flex: 1
-    },
-  });
+  container: {
+    justifyContent: "center",
+    fontSize: 30,
+    padding: 30,
+    backgroundColor: "#4287f5",
+    flex: 1
+  },
+
+  buttonContainer: {
+    backgroundColor: '#FFFFFF',
+    paddingVertical: 15,
+    borderWidth: 1,
+    borderRadius: 10,
+    borderColor: '#ddd',
+    borderBottomWidth: 0,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.8,
+    shadowRadius: 2,
+    elevation: 1,
+    marginLeft: 5,
+    marginRight: 5,
+    marginTop: 10,
+    paddingTop: 15,
+    paddingBottom: 15,
+},
+  buttonText: {
+    textAlign: 'center',
+    color: '#000000',
+    fontSize: 18,
+    fontWeight: 'bold',
+  }
+});
